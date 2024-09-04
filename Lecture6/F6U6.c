826d57fc53090e4bf114aeaafb05530aff684ec9
Kasiem Saeed <kasiemsaeed@gmail.com>
2024-09-04 21:08:15 +0200

    completed assignment for exercise 7

--- Lecture6/F6U6.c
+++ Lecture6/F6U6.c
@@ -2,45 +2,33 @@
 
 int main()
 {
-    int digitsArray[5][5], rowTotalsArray[5] = {0, 0, 0, 0, 0}, columnTotalsArray[5] = {0, 0, 0, 0, 0};
+    int rowTotalsArray[5] = {0, 0, 0, 0, 0}, columnTotalsArray[5] = {0, 0, 0, 0, 0};
 
     for (int columnCounter = 0; columnCounter < 5; columnCounter++)
     {
         printf("Enter row %d: ", (columnCounter + 1));
         for (int rowCounter = 0; rowCounter < 5; rowCounter++)
         {
-            scanf("%d", &digitsArray[columnCounter][rowCounter]);
-        }
+            int inputDigit = 0;
+            scanf("%d", &inputDigit);
 
-        // scanf("%d %d %d %d %d",
-        //       &digitsArray[columnCounter][0], &digitsArray[columnCounter][1],
-        //       &digitsArray[columnCounter][2], &digitsArray[columnCounter][3],
-        //       &digitsArray[columnCounter][4]);
-    }
-
-    for (int columnCounter = 0; columnCounter < 5; columnCounter++)
-    {
-        printf("\n");
-        for (int rowCounter = 0; rowCounter < 5; rowCounter++)
-        {
-            rowTotalsArray[columnCounter] = rowTotalsArray[columnCounter] + digitsArray[columnCounter][rowCounter];
+            rowTotalsArray[columnCounter] = rowTotalsArray[columnCounter] + inputDigit;
+            columnTotalsArray[rowCounter] = columnTotalsArray[rowCounter] + inputDigit;
         }
     }
 
-    for (int columnCounter = 0; columnCounter < 5; columnCounter++)
-    {
-        printf("\n");
-        for (int rowCounter = 0; rowCounter < 5; rowCounter++)
-        {
-            printf("%d", digitsArray[columnCounter][rowCounter]);
-        }
-    }
-
-    printf("\n");
+    printf("\nRow totals: ");
 
     for (int columnCounter = 0; columnCounter < 5; columnCounter++)
     {
         printf("%d ", rowTotalsArray[columnCounter]);
+    }
+
+    printf("\nColumn totals: ");
+
+    for (int columnCounter = 0; columnCounter < 5; columnCounter++)
+    {
+        printf("%d ", columnTotalsArray[columnCounter]);
     }
 
     return 0;
