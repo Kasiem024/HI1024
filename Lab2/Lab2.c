b4c83fde9fce6dce4a9ee4ed5154356913fe00d0
Kasiem Saeed <kasiemsaeed@gmail.com>
2024-09-15 16:23:22 +0200

    finished lab 2

--- Lab2/Lab2.c
+++ Lab2/Lab2.c
@@ -1,43 +1,56 @@
 #include <stdio.h>
+#include <string.h>
+#include <math.h>
 
-int viewStats();
-int enterStats(int *statsArray, int amountOfMeasurements);
-int computeStats();
-int resetStats();
+void viewMeasurements(int *measurementsArray, int amountOfMeasurements);
+int enterMeasurements(int *measurementsArray, int amountOfMeasurements);
+void computeMeasurements(int *measurementsArray, int amountOfMeasurements);
+int findMaxMeasurement(int *copyArray, int amountOfMeasurements);
+int findMinMeasurement(int *copyArray, int amountOfMeasurements);
+float findAvrMeasurement(int *copyArray, int amountOfMeasurements);
+void findNormalizedMeasurement(int *copyArray, int amountOfMeasurements);
 
 int main()
 {
     char userChoice = 'e';
-    int statsArray[10], amountOfMeasurements = 0;
+    int measurementsArray[10], amountOfMeasurements = 0;
+    printf("Measurement tool 2.0\n");
 
     while (userChoice != 'q')
     {
-        printf("\nVECRQ? ");
+        printf("VECRQ? ");
         scanf(" %c", &userChoice);
 
         switch (userChoice)
         {
         case 'v':
-            printf(" %c ", userChoice);
-
+            viewMeasurements(measurementsArray, amountOfMeasurements);
             break;
+
         case 'e':
-            amountOfMeasurements = enterStats(statsArray, amountOfMeasurements);
+            amountOfMeasurements = enterMeasurements(measurementsArray, amountOfMeasurements);
             break;
+
         case 'c':
-            printf(" %c ", userChoice);
+            computeMeasurements(measurementsArray, amountOfMeasurements);
 
             break;
+
         case 'r':
-            printf(" %c ", userChoice);
+            for (int counter = 0; counter < amountOfMeasurements; counter++)
+            {
+                measurementsArray[counter] = 0;
+            }
+            amountOfMeasurements = 0;
 
             break;
+
         case 'q':
-            printf("\nExiting ", userChoice);
-
+            printf("Exit Measurement tool", userChoice);
             break;
+
         default:
-            printf("\nWrong input try again ");
+            printf("Wrong input try again");
             break;
         }
     }
@@ -45,34 +58,48 @@ int main()
     return 0;
 }
 
-int enterStats(int *statsArray, int amountOfMeasurements)
+void viewMeasurements(int *measurementsArray, int amountOfMeasurements)
 {
-    char newMeasurement = 0;
+    if (amountOfMeasurements == 0)
+    {
+        printf("No measurements\n");
+    }
+    else
+    {
+        printf("[ ");
+        for (int counter = 0; counter < amountOfMeasurements; counter++)
+        {
+            printf("%d ", measurementsArray[counter]);
+        }
+        printf("]\n");
+    }
+}
+
+int enterMeasurements(int *measurementsArray, int amountOfMeasurements)
+{
+    int newMeasurement = 0;
+    char input[10];
 
     while (amountOfMeasurements < 10)
     {
-        printf("\nEnter measurement #%d (or 'q' to quit): ", (amountOfMeasurements + 1));
-        scanf(" %c", &newMeasurement);
-        printf("\nmeasurement d= %d", newMeasurement);
-        printf("\nmeasurement c= %c", newMeasurement);
+        printf("Enter measurement #%d (or q to quit): ", (amountOfMeasurements + 1));
+        scanf("%s", &input);
 
-        int temp = newMeasurement - '0';
-        printf("\ntemp d= %d", temp);
-        printf("\ntemp c= %c", temp);
-
-        if (newMeasurement == 'q')
+        if (strcmp(input, "q") == 0)
         {
-            printf("%d", newMeasurement);
-            printf("\nExiting e");
-
             break;
+        }
+
+        int successBool = sscanf(input, "%d", &newMeasurement);
+
+        if (successBool)
+        {
+            measurementsArray[amountOfMeasurements] = newMeasurement;
+            amountOfMeasurements++;
         }
         else
         {
-            printf("\nInputting %d", temp);
-
-            statsArray[amountOfMeasurements] = temp;
-            amountOfMeasurements++;
+            printf("\nInvalid input. Please enter a number or 'q' to quit.");
         }
     }
 
@@ -81,10 +108,94 @@ int enterStats(int *statsArray, int amountOfMeasurements)
         printf("\nYou already have 10 measurements!");
     }
 
-    for (int counter = 0; counter < amountOfMeasurements; counter++)
+    return amountOfMeasurements;
+}
+
+void computeMeasurements(int *measurementsArray, int amountOfMeasurements)
+{
+    if (amountOfMeasurements == 0)
     {
-        printf("%d ", statsArray[counter]);
+        printf("No measurements\n");
+    }
+    else
+    {
+        int copyArray[10];
+
+        for (int counter = 0; counter < amountOfMeasurements; counter++)
+        {
+            copyArray[counter] = measurementsArray[counter];
+        }
+
+        printf("Max mesaurement: %d\n", findMaxMeasurement(copyArray, amountOfMeasurements));
+        printf("Min mesaurement: %d\n", findMinMeasurement(copyArray, amountOfMeasurements));
+        printf("Avr mesaurement: %0.2f\n", findAvrMeasurement(copyArray, amountOfMeasurements));
+
+        for (int counter = 0; counter < amountOfMeasurements; counter++)
+        {
+            copyArray[counter] = measurementsArray[counter];
+        }
+
+        findNormalizedMeasurement(copyArray, amountOfMeasurements);
+        viewMeasurements(copyArray, amountOfMeasurements);
+    }
+}
+
+int findMaxMeasurement(int *copyArray, int amountOfMeasurements)
+{
+    for (int i = 0; i < amountOfMeasurements - 1; i++)
+    {
+        for (int counter = 0; counter < amountOfMeasurements - 1; counter++)
+        {
+            if (copyArray[counter] < copyArray[counter + 1])
+            {
+                int temp = copyArray[counter];
+                copyArray[counter] = copyArray[counter + 1];
+                copyArray[counter + 1] = temp;
+            }
+        }
     }
 
-    return amountOfMeasurements;
+    return copyArray[0];
+}
+
+int findMinMeasurement(int *copyArray, int amountOfMeasurements)
+{
+    for (int i = 0; i < amountOfMeasurements - 1; i++)
+    {
+        for (int counter = 0; counter < amountOfMeasurements - 1; counter++)
+        {
+            if (copyArray[counter] > copyArray[counter + 1])
+            {
+                int temp = copyArray[counter];
+                copyArray[counter] = copyArray[counter + 1];
+                copyArray[counter + 1] = temp;
+            }
+        }
+    }
+
+    return copyArray[0];
+}
+
+float findAvrMeasurement(int *copyArray, int amountOfMeasurements)
+{
+    float avrMesaurement = 0;
+
+    for (int counter = 0; counter < amountOfMeasurements; counter++)
+    {
+        avrMesaurement = avrMesaurement + copyArray[counter];
+    }
+
+    avrMesaurement = avrMesaurement / amountOfMeasurements;
+
+    return avrMesaurement;
+}
+
+void findNormalizedMeasurement(int *copyArray, int amountOfMeasurements)
+{
+    int avrMesaurement = round(findAvrMeasurement(copyArray, amountOfMeasurements));
+
+    for (int counter = 0; counter < amountOfMeasurements; counter++)
+    {
+        copyArray[counter] = copyArray[counter] - avrMesaurement;
+    }
 }
