4dfdc4a0a1d9671ec754973fa1d54027b17564de
Kasiem <kasiemsaeed@gmail.com>
2024-09-25 09:52:03 +0200

    finished 12_7, renamed struct in 12_6

--- Lecture12/F12U6.c
+++ Lecture12/F12U6.c
@@ -1,21 +1,22 @@
 #include <stdio.h>
 #include <math.h>
 
-struct ComplexNumber
+struct complexNumber
 {
     float real;
     float imaginary;
 };
+typedef struct complexNumber ComplexNumber;
 
-struct ComplexNumber funcCreateComplex(float real, float imaginary);
-void funcPrintComplex(struct ComplexNumber number);
-void funcMultiplyComplex(struct ComplexNumber *complexNumber, float decimalNumber);
-int funcEquals(struct ComplexNumber complexNumber1, struct ComplexNumber complexNumber2);
+ComplexNumber funcCreateComplex(float real, float imaginary);
+void funcPrintComplex(ComplexNumber number);
+void funcMultiplyComplex(ComplexNumber *complexNumber, float decimalNumber);
+int funcEquals(ComplexNumber complexNumber1, ComplexNumber complexNumber2);
 
 int main()
 {
-    struct ComplexNumber c1 = funcCreateComplex(3, +2);
-    struct ComplexNumber c2 = funcCreateComplex(2, -3);
+    ComplexNumber c1 = funcCreateComplex(3, +2);
+    ComplexNumber c2 = funcCreateComplex(2, -3);
 
     funcMultiplyComplex(&c1, -2.5);
     funcMultiplyComplex(&c2, -1);
@@ -28,9 +29,9 @@ int main()
     return 0;
 }
 
-struct ComplexNumber funcCreateComplex(float real, float imaginary)
+ComplexNumber funcCreateComplex(float real, float imaginary)
 {
-    struct ComplexNumber newComplexNumber;
+    ComplexNumber newComplexNumber;
 
     newComplexNumber.real = real;
     newComplexNumber.imaginary = imaginary;
@@ -38,7 +39,7 @@ struct ComplexNumber funcCreateComplex(float real, float imaginary)
     return newComplexNumber;
 }
 
-void funcPrintComplex(struct ComplexNumber number)
+void funcPrintComplex(ComplexNumber number)
 {
     char temp = '+';
 
@@ -51,13 +52,13 @@ void funcPrintComplex(struct ComplexNumber number)
     printf("\n%0.4f %c %0.4fi", number.real, temp, number.imaginary);
 }
 
-void funcMultiplyComplex(struct ComplexNumber *complexNumber, float decimalNumber)
+void funcMultiplyComplex(ComplexNumber *complexNumber, float decimalNumber)
 {
     complexNumber->real = complexNumber->real * decimalNumber;
     complexNumber->imaginary = complexNumber->imaginary * decimalNumber;
 }
 
-int funcEquals(struct ComplexNumber complexNumber1, struct ComplexNumber complexNumber2)
+int funcEquals(ComplexNumber complexNumber1, ComplexNumber complexNumber2)
 {
     if (complexNumber1.real == complexNumber2.real && complexNumber1.imaginary == complexNumber2.imaginary)
     {
--- Lecture12/F12U7.c
+++ Lecture12/F12U7.c
@@ -1,29 +1,83 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
-struct PlayingCard
+struct card
 {
     int number;
     char color;
 };
+typedef struct card Card;
 
-struct Hand
+struct hand
 {
-    char cards[52];
+    Card cards[52];
     int amount;
 };
+typedef struct hand Hand;
+
+void printHand(Hand currentHand);
+void swap(Hand *firstHand, Hand *secondHand);
 
 int main()
 {
-    struct Hand myHand;
-    myHand.amount = 4;
-    char myHandColorsArray[4] = {'s', 'h', 'r', 'r'};
-    int myHandNumbersArray[4] = {2, 12, 3, 8};
+    Hand myHand = {{{2, 's'},
+                    {12, 'h'},
+                    {3, 'r'},
+                    {8, 'r'}},
+                   4};
 
-    for (int counter = 0; counter < myHand.amount; counter++)
+    Hand yourHand = {{{3, 'k'},
+                      {4, 'k'},
+                      {5, 'k'},
+                      {6, 'k'},
+                      {7, 'k'}},
+                     5};
+
+    printHand(myHand);
+    printHand(yourHand);
+
+    swap(&myHand, &yourHand);
+
+    printHand(myHand);
+    printHand(yourHand);
+
+    return 0;
+}
+
+void printHand(Hand currentHand)
+{
+    for (int counter = 0; counter < currentHand.amount; counter++)
     {
+        printf("%c %d, ", currentHand.cards[counter].color, currentHand.cards[counter].number);
+    }
+    printf("\n");
+}
+
+void swap(Hand *firstHand, Hand *secondHand)
+{
+    srand(time(NULL));
+
+    int randomNum = rand() % firstHand->amount;
+
+    secondHand->cards[secondHand->amount].color = firstHand->cards[randomNum].color;
+    secondHand->cards[secondHand->amount].number = firstHand->cards[randomNum].number;
+
+    secondHand->amount++;
+
+    for (int counter = 0; counter < firstHand->amount; counter++)
+    {
+        int foundCard = 0;
+        if (firstHand->cards[counter].color == firstHand->cards[randomNum].color && firstHand->cards[counter].number == firstHand->cards[randomNum].number)
+        {
+            foundCard = 1;
+        }
+        if (foundCard == 1)
+        {
+            firstHand->cards[counter].color = firstHand->cards[counter + 1].color;
+            firstHand->cards[counter].number = firstHand->cards[counter + 1].number;
+        }
     }
 
-    struct Hand yourHand;
-    yourHand.amount = 5;
-    return 0;
+    firstHand->amount--;
 }
